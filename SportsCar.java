d5f9b2374e9bd976a61ad8497d73f00d6f71b610
Nathan Vo <115041909+nathanv0@users.noreply.github.com>
2025-02-11 00:28:57 -0600

    Delete SportsCar.java

--- SportsCar.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.time.LocalDate;
-
-public class SportsCar extends Vehicle {
-    public SportsCar(String vehicleId, String manufacture, String model, LocalDate acquisitionDate, double price) {
-        super(vehicleId, manufacture, model, acquisitionDate, price, "sports car");
-    }
-}
