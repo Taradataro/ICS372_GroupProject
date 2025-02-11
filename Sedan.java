5ebe472e392b7a7a554a4528e160ea2b7ffe3984
AhlamMahi96 <tc0201iw@go.minnstate.edu>
2025-02-11 00:29:01 -0600

    Delete Sedan.java

--- Sedan.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.time.LocalDate;
-
-public class Sedan extends Vehicle {
-    public Sedan(String vehicleId, String manufacture, String model, LocalDate acquisitionDate, double price) {
-        super(vehicleId, manufacture, model, acquisitionDate, price, "sedan");
-    }
-}
