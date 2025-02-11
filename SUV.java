7a33030668f640c80d2c91d95a9c839e25e3c7dc
AhlamMahi96 <tc0201iw@go.minnstate.edu>
2025-02-11 00:28:36 -0600

    Delete SUV.java

--- SUV.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.time.LocalDate;
-
-public class SUV extends Vehicle {
-    public SUV(String vehicleId, String manufacture, String model, LocalDate acquisitionDate, double price) {
-        super(vehicleId, manufacture, model, acquisitionDate, price, "suv");
-    }
-}
