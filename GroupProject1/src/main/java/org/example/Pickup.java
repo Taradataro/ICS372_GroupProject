26ca61dc53367dde1fa83c24c3a0904db2b3540f
Trang <taratrangnguyen99@gmail.com>
2025-02-12 09:55:17 -0600

    Add small change into Json export by making "userenable" to print out correct format using pretty printing. Also import org.Json into pom.xml

--- GroupProject1/src/main/java/org/example/Pickup.java
+++ GroupProject1/src/main/java/org/example/Pickup.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Pickup extends Vehicle{
+public class Pickup extends Vehicle {
     //Represents a Pick up. This class extends the Vehicle class and inherits its properties
     public Pickup(String vehicleId, String manufacture, String model, String acquisitionDate, double price) {
         super(vehicleId, manufacture, model, acquisitionDate, price);
