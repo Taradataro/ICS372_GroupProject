28166ac983b705b3c70d5b0d71f72ba5d48077e9
Trang <taratrangnguyen99@gmail.com>
2025-03-30 00:31:02 -0500

    testing

---
+++ GroupProject2/src/test/java/com/example/vehicletracking/DealerTest.java
@@ -0,0 +1,24 @@
+package com.example.vehicletracking;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DealerTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getVehicles() {
+    }
+
+    @Test
+    void addVehicle() {
+    }
+
+    @Test
+    void setVehicles() {
+    }
+}
